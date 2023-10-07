f02577959bebb82bae50b03378e6b3862a3edabf
ILowriderI <126571170+ILowriderI@users.noreply.github.com>
2023-10-08 02:51:34 +0300

    add save state to strage

--- task3/src/components/App/App.jsx
+++ task3/src/components/App/App.jsx
@@ -15,34 +15,46 @@ function App() {
     const updatedData = [...jsonData];
     updatedData[index].selected = !updatedData[index].selected;
     setJsonData(updatedData);
+    localStorage.setItem("storageJsonData", JSON.stringify(updatedData));
     const count = updatedData.filter((item) => item.selected).length;
     setSelectedCount(count);
+    localStorage.setItem("storageSelected", JSON.stringify(count));
   };
 
   const onClickResetData = () => {
     setJsonData([]);
     setSelectedCount(0);
+    localStorage.clear();
   };
 
   const onClikSendRequest = () => {
     axios
       .get(`http://universities.hipolabs.com/search?name=${inputValue}`)
-      .then(({ data }) => setJsonData(data))
+      .then(({ data }) => {
+        setJsonData(data);
+        localStorage.setItem("storageJsonData", JSON.stringify(data));
+      })
       .catch((error) => console.error(error));
   };
-  useEffect(() => {
 
-  }, [jsonData]);
+  useEffect(() => {}, [jsonData]);
+  useEffect(() => {
+    if (localStorage.getItem("storageJsonData") !== null) {
+      setJsonData(JSON.parse(localStorage.getItem("storageJsonData")));
+      setSelectedCount(JSON.parse(localStorage.getItem("storageSelected")));
+    }
+  }, []);
 
   return (
-    <div >
+    <div>
       <div className={styles.upper_box}>
         <SendRequest onSendRequest={onClikSendRequest} />
         <p>Selected: {selectedCount}</p>
       </div>
       <Table data={jsonData} onCheckboxChange={handleCheckboxChange} />
-      <div className={styles.reset}><button  onClick={onClickResetData}>Reset</button></div>
-      
+      <div className={styles.reset}>
+        <button onClick={onClickResetData}>Reset</button>
+      </div>
     </div>
   );
 }
